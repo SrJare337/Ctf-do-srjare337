a640e1d05e59f064657f14f094e18a8c887a3a70
𝐒𝐫 𝐉𝐚r𝐞𝟑𝟑𝟕 <srjare337@gmail.com>
2024-07-21 13:36:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,15 +4,12 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    // Criptografando senha usando MD5
     const encryptedPassword = CryptoJS.MD5(password).toString();
 
-    // Verificando login (usuário: 'admin', senha: '1a1dc91c907325c69271ddf0c944bc72' que é 'pass' em MD5)
     if (username === 'admin' && encryptedPassword === '1a1dc91c907325c69271ddf0c944bc72') {
         document.getElementById('message').textContent = 'Login bem-sucedido!';
         document.getElementById('message').style.color = 'green';
 
-        // Disparar confetes
         confetti({
             particleCount: 100,
             spread: 70,
